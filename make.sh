c9b73863b9956382be6c7bdaf9cc481bae0d7159
Zachary Clark <zach.clark@dornerworks.com>
2020-02-21 15:56:24 -0500

    Seriously, fail the build already!

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #gcc -o hellomake hellomake.c hellofunc.c -I.
 echo Hello!!! I\'m a shell script!!!
-make
 echo This build will fail... Silly computer doesn\'t have gcc...
+echo Maybe if I put make at the end of the file???
+make
