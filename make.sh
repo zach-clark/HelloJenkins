e6252f732adefb65848c2ba1982bc32a3c299aa2
Zachary Clark <zach.clark@dornerworks.com>
2020-02-21 15:41:40 -0500

    Creates a branching message

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
 #gcc -o hellomake hellomake.c hellofunc.c -I.
 echo Hello!!! I\'m a shell script!!!
+echo I can even make a new branch, and in theory, this should also \"build\", aka, run this script. Yippee!
