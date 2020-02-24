7e22dc2cb529d6cee74a7266f4ba5decb226c506
Zachary Clark <Zach.Clark@dornerworks.com>
2020-02-24 14:23:15 -0500

    Okay, stop making the build fail now

--- make.sh
+++ make.sh
@@ -5,4 +5,4 @@ echo This build will fail... Silly computer doesn\'t have gcc...
 echo Maybe if I put make at the end of the file???
 echo I\'m putting in this change to see if Jenkins will send an email if I put capital letters in my email, since it wasn\'t working with lowercase letters
 echo Alright, here\'s attempt two at actually getting emails.
-make
+#make
