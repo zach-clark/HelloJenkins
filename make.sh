3e843ec7a682cb64ea70d004bba99edd367a1bb1
zachclark <Zach.Clark@dornerworks.com>
2020-02-24 14:10:01 -0500

    Attempt 2 at getting emails

--- make.sh
+++ make.sh
@@ -4,4 +4,5 @@ echo I can even make a new branch, and in theory, this should also \"build\", ak
 echo This build will fail... Silly computer doesn\'t have gcc...
 echo Maybe if I put make at the end of the file???
 echo I\'m putting in this change to see if Jenkins will send an email if I put capital letters in my email, since it wasn\'t working with lowercase letters
+echo Alright, here\'s attempt two at actually getting emails.
 make
