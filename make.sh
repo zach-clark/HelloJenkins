5dde9cdc129ea5c5cd315f14075d4d948a983fc8
Zachary Clark <Zach.Clark@dornerworks.com>
2020-02-24 14:02:03 -0500

    Pushes a change with email using capital letters so I might get an email this time

--- make.sh
+++ make.sh
@@ -3,4 +3,5 @@ echo Hello!!! I\'m a shell script!!!
 echo I can even make a new branch, and in theory, this should also \"build\", aka, run this script. Yippee!
 echo This build will fail... Silly computer doesn\'t have gcc...
 echo Maybe if I put make at the end of the file???
+echo I\'m putting in this change to see if Jenkins will send an email if I put capital letters in my email, since it wasn\'t working with lowercase letters
 make
